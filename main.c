ea43369df2b6d228670cf95477117fe6ee631722
NetKBs <abijose535@gmail.com>
2023-02-24 10:55:25 -0400

    función para crear administrador completa + validaciones de contraseña

--- main.c
+++ main.c
@@ -15,6 +15,8 @@ int main() {
     if (stat("./data/superuser/admin", &file_stat) != true || file_stat.st_size <= 0 ) {
     	crearAdmin();
     }
+	
+
 
 	return 0;
 }
@@ -26,7 +28,8 @@ void crearAdmin() {
 	fp = fopen("./data/superuser/admin", "w+");
 
 	printf("Inserte clave de administrador: \n");
-	printf("Minimo 8 caracteres y maximo 20\n");
+	printf("Minimo 8 caracteres y maximo 20\n Debe contener minusculas y mayusculas\n");
+	printf("Debe contener numeros\n");
 	gets(clave);
 
 	// Verificar validez
@@ -42,5 +45,34 @@ void crearAdmin() {
 		system("pause");
 		return crearAdmin();
 	}
-
+	
+	short mayus = 0, minus = 0;
+	short nums = 0;
+	
+	for (int i = 0; i < strlen(clave); i++) {
+		
+		// Existen mayúsculas y minúsculas
+		if (clave[i] >= 'A' && clave[i] <= 'Z' && mayus == 0) {
+			mayus ++;
+				
+		} else if (clave[i] >= 'a' && clave[i] <= 'z' && minus == 0) {
+			minus ++;			
+		} 
+		// Existen números
+		if (clave[i] >= '0' && clave[i] <= '9' && nums == 0) {
+			nums ++;
+		}
+	}
+	
+	if (mayus == 0 || minus == 0 || nums == 0) {
+		printf("Debe contener mayusculas, minusculas y numeros");
+		system("pause");
+		return crearAdmin();
+	}
+	
+	fprintf(fp, clave);
+	fclose(fp);
+	printf("Cuenta de administrador creada con exito\n");
+	printf("Clave asignada: %s\n-No Perderla Bajo Ninguna Circunstancia-\n", clave);
+	system("pause");
 }
